5efe9de8b1ae92b295e34f69265eb3118be503d8
0112358132134-2026 <55027767+0112358132134-2026@users.noreply.github.com>
2020-11-22 16:41:04 -0600

    Recorte de código
    Generador de llaves

--- CHAT/Structs Final Project/SDES.cs
+++ CHAT/Structs Final Project/SDES.cs
@@ -6,11 +6,11 @@ namespace Structs_Final_Project
 {
     public class SDES
     {
-        public int[] P10position = { 7, 0, 9, 6, 3, 5, 2, 4, 8, 1};
-        public int[] P8position = { 6,4,9,8,1,3,0,5};
-        public int[] P4position = { 2,0,1,3};
-        public int[] EPposition = { 1,0,3,2,0,2,3,1};
-        public int[] IPposition = { 2,7,0,5,1,6,3,4};
+        public int[] P10_position = { 7, 0, 9, 6, 3, 5, 2, 4, 8, 1};
+        public int[] P8_position = { 6, 4, 9, 8, 1, 3, 0, 5};
+        public int[] P4_position = { 2, 0, 1, 3};
+        public int[] EP_position = { 1, 0, 3, 2, 0, 2, 3, 1};
+        public int[] IP_position = { 2, 7, 0, 5, 1, 6, 3, 4};
         public string k1,k2;
 
         public void KeysGenerator(string principalKey)
@@ -20,32 +20,33 @@ namespace Structs_Final_Project
                 string newPrincipalKey = Fill(principalKey);
 
                 string P10 = "";
-                for (int i = 0; i < P10position.Length; i++)
+                for (int i = 0; i < P10_position.Length; i++)
                 {
-                    P10 += newPrincipalKey[P10position[i]].ToString();
+                    P10 += newPrincipalKey[P10_position[i]].ToString();
                 }
 
-                string subdiv1 = P10.Substring(0, 5);
-                string subdiv2 = P10.Substring(5, 5);
-                string LS1subdiv1 = subdiv1.Substring(1, 4) + subdiv1[0].ToString();
-                string LS1subdiv2 = subdiv2.Substring(1, 4) + subdiv2[0].ToString();
-                string LS1 = LS1subdiv1 + LS1subdiv2;
-
-                for (int i = 0; i < P8position.Length; i++)
-                {
-                    k1 += LS1[P8position[i]].ToString();
-                }
+                string subDiv1 = P10.Substring(0, 5), subDiv2 = P10.Substring(5, 5);
+                string LS1subDiv1 = subDiv1.Substring(1, 4) + subDiv1[0].ToString();
+                string LS1subDiv2 = subDiv2.Substring(1, 4) + subDiv2[0].ToString();
+                string LS1 = LS1subDiv1 + LS1subDiv2;
                 string LS2 = LS1.Substring(2, 8) + LS1[0].ToString() + LS1[1].ToString();
-                
-                for (int i = 0; i < P8position.Length; i++)
+
+                for (int i = 0; i < P8_position.Length; i++)
                 {
-                    k2 += LS2[P8position[i]].ToString();
+                    k1 += LS1[P8_position[i]].ToString();
+                }                
+
+                for (int i = 0; i < P8_position.Length; i++)
+                {
+                    k2 += LS2[P8_position[i]].ToString();
                 }
             }
         }
-        public bool IsBinary (string principalKey)
+
+        public bool IsBinary(string principalKey)
         {
             bool result = false;
+
             if (principalKey.Length<=10) 
             {
                 for (int i = 0; i < principalKey.Length; i++)
@@ -59,11 +60,13 @@ namespace Structs_Final_Project
             }
             return result;
         }
+
         public string Fill(string principalKey)
         {
             StringBuilder aux = new StringBuilder();
             aux.Append(principalKey);
-            if (principalKey.Length<10)
+
+            if (principalKey.Length < 10)
             {
                 int remaining = 10 - principalKey.Length;
                 for (int i = 0; i < remaining; i++)
