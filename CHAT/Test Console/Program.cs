8e5e1ee075e43830d126f2790f6b5a309c3ef4d9
0112358132134-2026 <55027767+0112358132134-2026@users.noreply.github.com>
2020-11-22 18:51:06 -0600

    Prototipo SDES terminado
    Funciona bien ;)

--- CHAT/Test Console/Program.cs
+++ CHAT/Test Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Structs_Final_Project;
 
 namespace Test_Console
@@ -6,11 +7,13 @@ namespace Test_Console
     class Program
     {
         static void Main(string[] args)
-        {
-            SDES prueba = new SDES();
-            prueba.KeysGenerator("10001000");
-            Console.WriteLine(prueba.k1 +"," + prueba.k2);
-            Console.Read();
+        {           
+            SDES _SDES = new SDES();
+            _SDES.KeysGenerator("10001000");
+            byte[] textoOriginal = { 23, 84, 14, 199, 200};
+            byte[] textoCifrado = _SDES.SDES_Encryption(textoOriginal, "cifrado");
+            byte[] textoDescifrado = _SDES.SDES_Encryption(textoCifrado, "descifrado");
+            Console.ReadLine();
         }
     }
 }
