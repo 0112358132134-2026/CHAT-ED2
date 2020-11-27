80e612d85413a121fb1fff67d72d28f719c642cf
Jocelynardon <jadeleonard@correo.url.edu.gt>
2020-11-27 12:25:13 -0600

    Prueba de hora
    Intentando arreglar la zona

--- CHAT/Design/Controllers/UserController.cs
+++ CHAT/Design/Controllers/UserController.cs
@@ -203,7 +203,7 @@ namespace Design.Controllers
             newMessage.receptor = form["ListUsers"];
             newMessage.message = form["Message"];
             newMessage._id = Guid.NewGuid().ToString();
-            newMessage.date = DateTime.Now;
+            newMessage.date = DateTime.;
             newMessage.type = "message";
 
             if ((newMessage.message != "") && (newMessage.receptor != ""))
--- CHAT/Test Console/Program.cs
+++ CHAT/Test Console/Program.cs
@@ -12,6 +12,12 @@ namespace Test_Console
             byte[] textoOriginal = { 23, 84, 14, 199, 200, 80, 88, 99, 40, 40, 41, 24, 42, 3};
             byte[] textoCifrado = _SDES.SDES_Encryption(textoOriginal, "cifrado");
             byte[] textoDescifrado = _SDES.SDES_Encryption(textoCifrado, "descifrado");
+
+            DateTime dateNow = DateTime.Now;
+            //dateNow.ToLocalTime();
+            Console.WriteLine("The date and time are {0} UTC.",
+                               TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateNow, "Central Standard Time"));
+
             Console.ReadLine();
         }
     }
